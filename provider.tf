532290f643d70906c58d7928c99950543948367b
Virendra <virendrasingh_saini@yahoo.com>
2023-10-27 12:25:09 +0530

    Cred Added

--- provider.tf
+++ provider.tf
@@ -9,5 +9,7 @@ terraform {
 
 provider "aws" {
   # profile = "aclg"
+  aws_access_key_id = AKIA5QB34A5NRMCZSF6N
+  aws_secret_access_key = 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
   region = "us-east-1"
 }
