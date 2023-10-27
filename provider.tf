7f1e3aded60521a4f8a5c861e0ef6c4cc622735e
Virendra <virendrasingh_saini@yahoo.com>
2023-10-27 15:13:22 +0530

    Cred Var Used10

--- provider.tf
+++ provider.tf
@@ -8,6 +8,9 @@ terraform {
 }
 
 provider "aws" {
+  aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+  aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  aws-region: ${{ secrets.AWS_REGION }}
   # profile = "aclg"
   # access_key = "AWS_ACCESS_KEY_ID"
   # access_key = "AKIA5QB34A5NRMCZSF6N"
