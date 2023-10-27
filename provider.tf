0b9217a0df5de60cafcf72e522a62ab879b71a61
Virendra <virendrasingh_saini@yahoo.com>
2023-10-27 13:53:32 +0530

    Cred Var Used1

--- provider.tf
+++ provider.tf
@@ -9,9 +9,9 @@ terraform {
 
 provider "aws" {
   # profile = "aclg"
-  access_key = "AWS_ACCESS_KEY_ID"
+  # access_key = "AWS_ACCESS_KEY_ID"
   # access_key = "AKIA5QB34A5NRMCZSF6N"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
+  # secret_key = "AWS_SECRET_ACCESS_KEY"
   # secret_key = "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"
   region = "us-east-1"
 }
