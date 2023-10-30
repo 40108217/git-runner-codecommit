a45e4069205af2d2e699a1956799eeb4f524a3ea
Virendra <virendrasingh_saini@yahoo.com>
2023-10-30 17:05:41 +0530

    Push

--- LandingZone/resources/provider.tf
+++ LandingZone/resources/provider.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "aws" {
-  #access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-  #secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-  #region = ${{ secrets.AWS_REGION }}
+  access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
+  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  region = ${{ secrets.AWS_REGION }}
   # profile = "aclg"
   # access_key = "AWS_ACCESS_KEY_ID"
   # access_key = "AKIA5QB34A5NRMCZSF6N"
