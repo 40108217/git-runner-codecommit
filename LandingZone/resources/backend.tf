b33a4df42639b07eff0170327c3f553399a69c67
Virendra <virendrasingh_saini@yahoo.com>
2023-11-14 12:27:09 +0530

    test

--- LandingZone/resources/backend.tf
+++ LandingZone/resources/backend.tf
@@ -1,6 +1,6 @@
 terraform{
     backend "s3" {
-        bucket = "40108217"
+        bucket = "40108217-tmp"
         key = "test.tfstate"
         region = "us-east-1"
         # profile = "default"
