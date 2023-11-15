e076d7aa5032d73f82f7ec277c2d3891accfb8c5
40108217 <virendrasingh_saini@yahoo.com>
2023-11-15 13:59:12 +0530

    test

--- LandingZone/resources/provider.tf
+++ LandingZone/resources/provider.tf
@@ -11,10 +11,10 @@ provider "aws" {
   #access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
   #secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
   #region = ${{ secrets.AWS_REGION }}
-  # profile = "aclg"
+  profile = "default"
   # access_key = "AWS_ACCESS_KEY_ID"
-  access_key = "AKIAZUA7TSGRCRXZBIGJ"
+  #access_key = "AKIAZUA7TSGRCRXZBIGJ"
   # secret_key = "AWS_SECRET_ACCESS_KEY"
-  secret_key = "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"
+  #secret_key = "SBnI5KatL8hIYATgtjfQ2XSnsscbsuaff3Gw+Pk+"
   region = "us-east-1"
 }
